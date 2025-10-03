567b4a6bb4d5e39d7b7b35ea9a729bc4d89664a4
samarjitsahoo <samarjit9203@gmail.com>
2025-10-03 23:02:50 +0530

    Initial Commit

---
+++ src/main/java/io/springboot/main/HomeController.java
@@ -0,0 +1,14 @@
+package io.springboot.main;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping({"/","/home","/status"})
+	public String getStatus() {
+		return "Application is up and running!";
+	}
+
+}
